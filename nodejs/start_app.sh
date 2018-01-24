ce9b26462bb026569ff22f328ac6a4333ba90a31
Steven Stevanus <stvn.org@gmail.com>
2018-01-24 15:05:38 +0700

    add redis Dockerfile

--- nodejs/start_app.sh
+++ nodejs/start_app.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 service nginx start
 npm install
-npm start > /dev/null 2>&1 & 
+#NODE_ENV=development pm2 start src/index.js npm -- start -i 1 -x -- --port 9000 -i 1
+npm start > /app/logs.txt &
+#npm start > /dev/null 2>&1 & 
+#pm2 start npm -- start
 tail -f /var/log/nginx/access.log

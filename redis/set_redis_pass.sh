01373ee538c5944ef251760670807fd67064ef6d
Steven Stevanus <stvn.org@gmail.com>
2018-02-09 09:55:47 +0700

    edit set_redis_pass.sh

--- redis/set_redis_pass.sh
+++ redis/set_redis_pass.sh
@@ -4,5 +4,4 @@ echo -ne '\n' | redis-server &
 redis-cli AUTH PASSWORD
 redis-cli CONFIG SET requirepass "0711dc4c4d7365e522ff56634e8102cf5863d5aba2818c5f02615e80ed9d347b"
 redis-cli AUTH 0711dc4c4d7365e522ff56634e8102cf5863d5aba2818c5f02615e80ed9d347b
-redis-cli -a 0711dc4c4d7365e522ff56634e8102cf5863d5aba2818c5f02615e80ed9d347b shutdown
-redis-server
+redis-cli monitor
